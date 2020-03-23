d1921afecdf27c50d086161e7d852b4fbf6df9c9
bagasnur <bagasnur00@mail.ugm.ac.id>
2020-03-23 19:27:23 +0700

    Tugas 2 - Card

---
+++ react-project-Card/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+const Card = (props) => {
+  return(
+    <div class="card">
+      <p class="title">{props.name}</p>
+      <p>{props.job}</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Card name="Abdurrahman Jaes" job="Front-End Web Developer"></Card>
+      <Card name="Bramantya Gentak" job="Back-End Web Developer"></Card>
+      <Card name="Muklas Muklis" job="Android Developer"></Card>
+      <Card name="Ruditya Hedon" job="UI/UX"></Card>
+    </div>
+  );
+}
+
+export default App;
