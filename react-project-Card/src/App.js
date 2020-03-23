a674c00916652cd0d4a96f0be5d03aff2af55569
bagasnur <bagasnur00@mail.ugm.ac.id>
2020-03-23 23:55:42 +0700

    Tugas 2 - Card

--- react-project-Card/src/App.js
+++ react-project-Card/src/App.js
@@ -14,9 +14,9 @@ function App() {
   return (
     <div className="App">
       <Card name="Abdurrahman Jaes" job="Front-End Web Developer"></Card>
-      <Card name="Bramantya Gentak" job="Back-End Web Developer"></Card>
+      <Card name="Bramantya Gento" job="Back-End Web Developer"></Card>
       <Card name="Muklas Muklis" job="Android Developer"></Card>
-      <Card name="Ruditya Hedon" job="UI/UX"></Card>
+      <Card name="Ruditya Hedon" job="UI/UX Designer"></Card>
     </div>
   );
 }
